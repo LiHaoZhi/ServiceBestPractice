c024b563068863ac63392821f2822c56b1d2bd5a
LiHaoZhi <1191926255@qq.com>
2019-12-25 16:42:11 +0800

    First commit.

---
+++ app/src/main/java/demoapp/smaato/com/myapplication/DownloadListener.java
@@ -0,0 +1,9 @@
+package demoapp.smaato.com.myapplication;
+
+public interface DownloadListener {
+    void onProgress(int progress);
+    void onSuccess();
+    void onFailed();
+    void onPaused();
+    void onCanceled();
+}
---
+++ app/src/main/java/demoapp/smaato/com/myapplication/Main2Activity.java
@@ -0,0 +1,28 @@
+package demoapp.smaato.com.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Main2Activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        TextView tv = findViewById(R.id.m2);
+
+
+    }
+
+
+}
